632feb90755bfeb6fc6af9f71516d971b156323d
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-02-08 10:17:11 -0700

    Created our tableviewCell in the main view

---
+++ Pokedex_Codable/Pokedex_Codable/Controllers/View Controllers/pokeDexTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  pokeDexTableViewCell.swift
+//  Pokedex_Codable
+//
+//  Created by Brody Sears on 2/8/22.
+//
+
+import UIKit
+
+class pokeDexTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
